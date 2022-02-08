570c6a1194677eb1476df084a2699912b63d79e2
Genadyy <bugutskyy@gmail.com>
2022-02-08 18:39:01 +0200

    change select

--- src/UI/selectors/MySelect.jsx
+++ src/UI/selectors/MySelect.jsx
@@ -9,7 +9,7 @@ const MySelect = (props) => {
     return (
         <div>
             <select id= {id} className={classes[className]} onChange={action}>
-                <option  defaultValue = 'Choose kind of contact' >Choose kind of contact</option>
+                <option  defaultValue = 'Choose kind of contact' >Choose kind of info</option>
                 <option value = 'date of birth'>Date of birth</option>
                 <option value = 'phone'>Phone</option>
                 <option value = 'e-mail'>E-mail</option>
